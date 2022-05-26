4e62360143b6f973b4ea9d579209156c81975cec
edrian0512 <mr.edrian@gmail.com>
2022-05-27 04:04:47 +0800

    Commit

--- public/js/script.js
+++ public/js/script.js
@@ -13,4 +13,25 @@ document.querySelectorAll('.about .video-container .controls .control-btn').forE
         let src = btn.getAttribute('data-src');
         document.querySelector('.about .video-container .video').src = src;
     }
-})
+});
+
+// animation
+AOS.init({
+    duration: 800,
+    offset: 150,
+});
+
+// Rates and Inclusion
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
